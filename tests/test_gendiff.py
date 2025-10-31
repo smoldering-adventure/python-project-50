144faecd8221e5c359106dbaaff78fa8aa23868f
Smoldering <m.p.w.200032@gmail.com>
2025-10-31 23:51:48 +0300

    add tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,1 +1,23 @@
+import os
+
+import pytest  # type: ignore
+
 from gendiff.main import generate_diff
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+
+file1 = os.path.join(test_dir, "test_data", "file1.json")
+file2 = os.path.join(test_dir, "test_data", "file2.json")
+file_result = os.path.join(test_dir, "test_data", "expected_diff_plain.txt")
+
+
+@pytest.fixture(scope="module")
+def expected_output() -> str:
+    """Фикстура для чтения эталонного вывода из файла."""
+    return open(file_result).read()
+
+
+def test_flat_json_diff(expected_output):
+    """Тестирует сравнение двух плоских JSON-файлов."""
+
+    assert generate_diff(file1, file2) == expected_output
